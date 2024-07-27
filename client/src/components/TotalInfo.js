5e06d223a86a86cbb84bac8111691b6c1871672f
dmytro-ustynov <ustynov.dmitriy@gmail.com>
2024-07-27 11:16:00 +0300

    Service names click performs filtering by service

--- client/src/components/TotalInfo.js
+++ client/src/components/TotalInfo.js
@@ -7,17 +7,24 @@ import Loader from "./loader";
 function TotalInfo() {
     const store = useContext(StoreContext)
     const {counters, pending} = store
+    const {filterItems, dropFilters} = store
+
+    const filterByServiceName = (key) => {
+        filterItems({service: SERVICES[key].alias.toUpperCase()})
+    }
 
     return (
         <div className={"search-handlers"}>
-            {pending? <Loader />: <>
-                <div className={"info-total"}>
+            {pending ? <Loader/> : <>
+                <div className="info-total as-anchor" onClick={() => dropFilters()}>
                     Всього: {counters.total}
                 </div>
                 {Object.keys(SERVICES).map((key) => {
                     return <div key={key}
-                                className={`info-${SERVICES[key].alias}`}>&#10687; {SERVICES[key].name}:
-                        - {counters[key]}</div>
+                                onClick={() => filterByServiceName(key)}
+                                className={`info-${SERVICES[key].alias}`}>&#10687;&#160;
+                        <span className="as-anchor">{SERVICES[key].name}:
+                        - {counters[key]} </span></div>
                 })}</>}
         </div>
     )
